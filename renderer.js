32f92b75d36d3c0f759c7d73d1ad1043945d5823
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-04-28 01:24:27 +0300

    Added more info

--- renderer.js
+++ renderer.js
@@ -11,7 +11,30 @@ let output = `
       <li class="list-group-item">Node: ${process.versions.node} </li>
       <li class="list-group-item">Chrome: ${process.versions.chrome} </li>
       <li class="list-group-item">Electron: ${process.versions.electron} </li>
+    </ul>
 
+    <h2 class="page-header">System Specs</h2>
+    <ul class="list-group">
+      <li class="list-group-item">System Architecture: ${process.arch} </li>
+      <li class="list-group-item">Processor Identifier: ${process.env.PROCESSOR_IDENTIFIER} </li>
+      <li class="list-group-item">Printer: ${process.env.PRINTER} </li>
+    </ul>
+
+    <h2 class="page-header">System Memroy</h2>
+    <ul class="list-group">
+      <li class="list-group-item">Total: ${process.getSystemMemoryInfo().total} </li>
+      <li class="list-group-item">Free: ${process.getSystemMemoryInfo().free} </li>
+      <li class="list-group-item">Swap Total: ${process.getSystemMemoryInfo().swapTotal} </li>
+      <li class="list-group-item">Swap Free: ${process.getSystemMemoryInfo().swapFree} </li>
+    </ul>
+
+    <h2 class="page-header">Computer Info</h2>
+    <ul class="list-group">
+      <li class="list-group-item">Computer Name: ${process.env.USERDOMAIN} </li>
+      <li class="list-group-item">Username: ${process.env.USERNAME} </li>
+      <li class="list-group-item">User Home Path: ${process.env.HOME} </li>
+      <li class="list-group-item">System Drive: ${process.env.SYSTEMDRIVE} </li>
+      <li class="list-group-item">System Root: ${process.env.SYSTEMROOT} </li>
     </ul>
     `
 
