a2dbe3c17835078e5cbf44b15719eb14e1426861
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-04-28 01:16:15 +0300

    Displayed some versions numbers

--- renderer.js
+++ renderer.js
@@ -4,3 +4,15 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+console.log(process)
+let output = `
+    <h2 class="page-header">App Version Data</h2>
+    <ul class="list-group">
+      <li class="list-group-item">Node: ${process.versions.node} </li>
+      <li class="list-group-item">Chrome: ${process.versions.chrome} </li>
+      <li class="list-group-item">Electron: ${process.versions.electron} </li>
+
+    </ul>
+    `
+
+    document.getElementById('output').innerHTML = output
